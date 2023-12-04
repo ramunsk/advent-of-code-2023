9d2e3a0cfacae87608699fce1fe6ec8978268e4e
Ramunas <514899+ramunsk@users.noreply.github.com>
2023-12-04 19:35:47 +0200

    (Day 03 Part 1) Good bye, Jest, Hello Vitest

--- src/day-02/support/lexer.spec.ts
+++ src/day-02/support/lexer.spec.ts
@@ -1,13 +1,14 @@
-import { Lexer, TokenType } from "./lexer";
+import { describe, expect, it } from 'vitest';
+import { Lexer, TokenType } from './lexer';
 
 describe('Lexer', () => {
     describe('should correctly tokenize', () => {
         describe('text', () => {
             const T = TokenType.Text;
             it.each`
-                text | expectedTokens
+                text                     | expectedTokens
                 ${'This is simple text'} | ${[[T, 'This', 0], [T, 'is', 5], [T, 'simple', 8], [T, 'text', 15]]}
-            `('$text', ({text, expectedTokens}) => {
+            `('$text', ({ text, expectedTokens }) => {
                 const tokens = new Lexer(text).tokenize();
                 expect(tokens).toHaveLength(expectedTokens.length + 1);
                 expectedTokens.forEach((expectedToken: [TokenType, string, number], i: number) => {
@@ -15,24 +16,24 @@ describe('Lexer', () => {
                     expect(tokens[i].value).toBe(expectedToken[1]);
                     expect(tokens[i].line).toBe(0);
                     expect(tokens[i].column).toBe(expectedToken[2]);
-                })
+                });
             });
         });
         describe('number', () => {
             const N = TokenType.Number;
             it.each`
-                text | expectedTokens
+                text            | expectedTokens
                 ${'1 5 12 278'} | ${[[N, '1', 0], [N, '5', 2], [N, '12', 4], [N, '278', 7]]}
-            `('$text', ({text, expectedTokens}) => {
+            `('$text', ({ text, expectedTokens }) => {
                 const tokens = new Lexer(text).tokenize();
                 expect(tokens).toHaveLength(expectedTokens.length + 1);
-                console.log(tokens)
+                console.log(tokens);
                 expectedTokens.forEach((expectedToken: [TokenType, string, number], i: number) => {
                     expect(tokens[i].type).toBe(expectedToken[0]);
                     expect(tokens[i].value).toBe(expectedToken[1]);
                     expect(tokens[i].line).toBe(0);
                     expect(tokens[i].column).toBe(expectedToken[2]);
-                })
+                });
             });
         });
     });
--- src/day-02/support/parser.spec.ts
+++ src/day-02/support/parser.spec.ts
@@ -1,3 +1,4 @@
+import { describe, expect, it } from 'vitest';
 import { Lexer } from './lexer';
 import { Parser } from './parser';
 
