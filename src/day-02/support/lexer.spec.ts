4104bbac0e59734894344160d3ffefe1961fbc61
Ramunas <514899+ramunsk@users.noreply.github.com>
2023-12-04 09:00:20 +0200

    (Day 02 Part 1) Fixed Lexer

--- src/day-02/support/lexer.spec.ts
+++ src/day-02/support/lexer.spec.ts
@@ -1,5 +1,39 @@
+import { Lexer, TokenType } from "./lexer";
+
 describe('Lexer', () => {
-    it('dummy test', () => {
-        expect(1).toBe(1);
+    describe('should correctly tokenize', () => {
+        describe('text', () => {
+            const T = TokenType.Text;
+            it.each`
+                text | expectedTokens
+                ${'This is simple text'} | ${[[T, 'This', 0], [T, 'is', 5], [T, 'simple', 8], [T, 'text', 15]]}
+            `('$text', ({text, expectedTokens}) => {
+                const tokens = new Lexer(text).tokenize();
+                expect(tokens).toHaveLength(expectedTokens.length + 1);
+                expectedTokens.forEach((expectedToken: [TokenType, string, number], i: number) => {
+                    expect(tokens[i].type).toBe(expectedToken[0]);
+                    expect(tokens[i].value).toBe(expectedToken[1]);
+                    expect(tokens[i].line).toBe(0);
+                    expect(tokens[i].column).toBe(expectedToken[2]);
+                })
+            });
+        });
+        describe('number', () => {
+            const N = TokenType.Number;
+            it.each`
+                text | expectedTokens
+                ${'1 5 12 278'} | ${[[N, '1', 0], [N, '5', 2], [N, '12', 4], [N, '278', 7]]}
+            `('$text', ({text, expectedTokens}) => {
+                const tokens = new Lexer(text).tokenize();
+                expect(tokens).toHaveLength(expectedTokens.length + 1);
+                console.log(tokens)
+                expectedTokens.forEach((expectedToken: [TokenType, string, number], i: number) => {
+                    expect(tokens[i].type).toBe(expectedToken[0]);
+                    expect(tokens[i].value).toBe(expectedToken[1]);
+                    expect(tokens[i].line).toBe(0);
+                    expect(tokens[i].column).toBe(expectedToken[2]);
+                })
+            });
+        });
     });
 });
--- src/day-02/support/lexer.ts
+++ src/day-02/support/lexer.ts
@@ -22,6 +22,7 @@ export class Lexer {
     #numberOfTokens: number;
     #line: number;
     #column: number;
+    #canMerge: boolean;
 
     constructor(source: string) {
         this.#source = source ?? '';
@@ -30,34 +31,39 @@ export class Lexer {
         this.#numberOfTokens = 0;
         this.#line = 0;
         this.#column = 0;
+        this.#canMerge = false;
     }
 
     tokenize(): Token[] {
         for (let position = 0; position < this.#sourceLength; position++) {
             const char = this.#source[position];
-            const lastToken = this.#lastToken;
 
             switch (char) {
                 case ' ':
-                    this.#column++;
                     // ignore whitespace
+                    this.#column++;
+                    this.#canMerge = false;
                     break;
                 case ':':
                     this.#addToken(TokenType.Colon, char);
                     this.#column++;
+                    this.#canMerge = false;
                     break;
                 case ';':
                     this.#addToken(TokenType.Semicolon, char);
                     this.#column++;
+                    this.#canMerge = false;
                     break;
                 case ',':
                     this.#addToken(TokenType.Comma, char);
                     this.#column++;
+                    this.#canMerge = false;
                     break;
                 case '\n':
                     this.#addToken(TokenType.NewLine, char);
                     this.#column = 0;
                     this.#line++;
+                    this.#canMerge = false;
                     break;
                 case '0':
                 case '1':
@@ -69,29 +75,22 @@ export class Lexer {
                 case '7':
                 case '8':
                 case '9':
-                    // TODO: Ramunas - continue here
-                    // Tests might be needed as token positions are messed up
-                    let num = char;
-                    let isNumber = false;
-                    do {
-                        const nextChar = this.#source.at(position + 1)!;
-                        isNumber = nextChar >= '0' && nextChar <= '9';
-                        if (isNumber) {
-                            num += nextChar;
-                            position++;
-                        }
-                        this.#column++;
-                    } while (isNumber === true);
-                    this.#addToken(TokenType.Number, num);
+                    if (this.#lastToken?.type === TokenType.Number && this.#canMerge) {
+                        this.#lastToken.value += char;
+                    } else {
+                        this.#addToken(TokenType.Number, char);
+                    }
+                    this.#column++;
+                    this.#canMerge = true;
                     break;
                 default:
                     // Text
-                    const previousChar = this.#source.at(position - 1);
-                    if (lastToken?.type === TokenType.Text && previousChar !== ' ') {
-                        lastToken.value += char;
+                    if (this.#lastToken?.type === TokenType.Text && this.#canMerge) {
+                        this.#lastToken.value += char;
                     } else {
                         this.#addToken(TokenType.Text, char);
                     }
+                    this.#canMerge = true;
                     this.#column++;
                     break;
             }
@@ -101,12 +100,12 @@ export class Lexer {
         return this.#tokens.slice(0, this.#numberOfTokens);
     }
 
-    #addToken(type: TokenType, value: string): void {
+    #addToken(type: TokenType, value: string, line?: number, column?: number): void {
         this.#tokens[this.#numberOfTokens] = {
             type,
             value,
-            line: this.#line,
-            column: this.#column,
+            line: line ?? this.#line,
+            column: column ?? this.#column,
         };
         this.#numberOfTokens++;
     }
