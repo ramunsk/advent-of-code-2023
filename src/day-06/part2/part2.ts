7c4c1f9345b227f2b9a4aa3cc4303223b0cc06f1
Ramunas <514899+ramunsk@users.noreply.github.com>
2023-12-06 07:56:08 +0200

    (Day 06 Part 2) Done

--- src/day-06/part2/part2.ts
+++ src/day-06/part2/part2.ts
@@ -1,2 +1,31 @@
 import { readFileSync } from 'fs';
-const fileContent = readFileSync('../sample.txt', { encoding: 'utf-8' });
+
+interface Race {
+    time: number;
+    distance: number;
+    waysToWin: number;
+}
+
+function readRace(): Race {
+    const fileContent = readFileSync('../input.txt', { encoding: 'utf-8' });
+    const [timeStr, distanceStr] = fileContent.split('\n').map((s) => s.replace('Time:', '').replace('Distance:', ''));
+    const time = Number(timeStr.replaceAll(' ', ''));
+    const distance = Number(distanceStr.replaceAll(' ', ''));
+
+    return { time, distance, waysToWin: 0 };
+}
+
+const race = readRace();
+// console.log(race);
+
+for (let i = 0; i < race.time; i++) {
+    const speed = i;
+    const timeLeft = race.time - i;
+    const distance = speed * timeLeft;
+    if (distance > race.distance) {
+        race.waysToWin++;
+    }
+}
+
+console.log('Ways to win:', race.waysToWin);
+// 30077773 is the right answer
