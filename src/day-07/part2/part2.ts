f04afac271ab9a642f7e5a0f0dab2abadfeed045
Ramunas <514899+ramunsk@users.noreply.github.com>
2023-12-08 20:24:31 +0200

    (Day 07 Part 2) Done

--- src/day-07/part2/part2.ts
+++ src/day-07/part2/part2.ts
@@ -1,2 +1,13 @@
 import { readFileSync } from 'fs';
-const fileContent = readFileSync('../sample.txt', { encoding: 'utf-8' });
+import { parseGames } from './game';
+
+const fileContent = readFileSync('../input.txt', { encoding: 'utf-8' });
+const games = parseGames(fileContent);
+games.sort((a, b) => a.hand.value.localeCompare(b.hand.value));
+games.forEach((g) => {
+    console.log(g.toString());
+});
+const winnings = games.reduce((prev, curr, i) => prev + curr.bid * (i + 1), 0);
+console.log('Winnings are:', winnings);
+// 250017938 is too high
+// 249400220 is correct answer
