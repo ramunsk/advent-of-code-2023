2c8fd348f407465e58f287064242169783564d35
Ramunas <514899+ramunsk@users.noreply.github.com>
2023-12-09 12:54:05 +0200

    (Day 09 Part 1) Done

---
+++ src/day-09/part1/part1.ts
@@ -0,0 +1,51 @@
+import { readFileSync } from 'fs';
+type Sample = number[];
+type Samples = Sample[];
+
+function __printArray(array: number[]) {
+    console.log(array.map((x) => x.toString()).join(' '));
+}
+
+function readSamples(input: string): Samples {
+    return input.split('\n').map((line) => line.split(' ').map((x) => Number(x)));
+}
+
+function extrapolateSample(sample: Sample) {
+    // __printArray(sample);
+    const diffs: Samples = [];
+    let diff: Sample;
+    do {
+        const prev = diffs.at(-1) ?? sample;
+        diff = new Array<number>(prev.length - 1);
+        for (let i = 0; i < diff.length; i++) {
+            diff[i] = prev[i + 1] - prev[i];
+        }
+        diffs.push(diff);
+    } while (!diff.every((x) => x === 0));
+
+    // diffs.forEach((diff) => {
+    //     __printArray(diff);
+    // });
+
+    diffs.at(-1)?.push(0);
+    for (let i = diffs.length - 2; i >= 0; i--) {
+        diffs[i].push(diffs[i].at(-1)! + diffs[i + 1].at(-1)!);
+    }
+
+    // diffs.forEach((diff) => {
+    //     __printArray(diff);
+    // });
+
+    sample.push(diffs[0].at(-1)! + sample.at(-1)!);
+    // __printArray(sample);
+}
+
+const fileContent = readFileSync('../input.txt', { encoding: 'utf-8' });
+const samples = readSamples(fileContent);
+// console.log(samples);
+samples.forEach((sample) => {
+    extrapolateSample(sample);
+});
+const total = samples.map((s) => s.at(-1)!).reduce((a, b) => a + b, 0);
+console.log('Total is:', total);
+// 2098530125 is the right answer
